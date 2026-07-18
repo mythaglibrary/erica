4c3a1c55c0b337f4a3e2f23dd460bb038984f327
mythaglibrary <samueledeluca@proton.me>
2026-07-18 10:17:44 +0200

    Update and rename banner.js to packs.js

--- commands/packs.js
+++ commands/packs.js
@@ -2,14 +2,14 @@ import { MessageFlags, SlashCommandBuilder } from "discord.js";
 import { readFile } from "fs/promises";
 
 const content = await readFile(
-  new URL(import.meta.resolve("content/banner.md")),
+  new URL(import.meta.resolve("content/packs.md")),
   "utf8",
 );
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("banners")
-    .setDescription("Anniversary banners and packs value breakdowns"),
+    .setName("packs")
+    .setDescription("Table of paid packs value"),
 
   async execute(interaction) {
     await interaction.reply({ content, flags: MessageFlags.SuppressEmbeds });
