59ec4d65d034e2a5e6a7c4e82db7b96bca3a0d9b
mythaglibrary <samueledeluca@proton.me>
2026-08-06 09:53:08 +0200

    Update character.js

--- commands/character.js
+++ commands/character.js
@@ -10,6 +10,10 @@ const guides = {
     new URL(import.meta.resolve("content/characters/castor.md")),
     "utf8",
   ),
+    pontos: await readFile(
+    new URL(import.meta.resolve("content/characters/pontos.md")),
+    "utf8",
+  ),
     clementine: await readFile(
     new URL(import.meta.resolve("content/characters/clementine.md")),
     "utf8",
@@ -84,6 +88,7 @@ export default {
         .addChoices(
           { name: "Arachne", value: "arachne" },
           { name: "Castor", value: "castor" },
+          { name: "Pontos", value: "pontos" },
           { name: "Glotan", value: "glotan" },
           { name: "Clementine", value: "clementine" },
           { name: "Corpo", value: "corpo" },
